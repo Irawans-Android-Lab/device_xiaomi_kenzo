aed27488c8ef9d764b83135c9a8465785ed70c55
Yaroslav Zviezda <acroreiser@gmail.com>
2024-07-01 18:43:13 +0300

    kenzo: camera: fix Camera HAL build for U
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -96,6 +96,7 @@ LOCAL_SHARED_LIBRARIES += libts_face_beautify_hal libts_detected_face_hal
 endif
 LOCAL_HEADER_LIBRARIES += camera_common_headers
 LOCAL_HEADER_LIBRARIES += display_headers
+LOCAL_HEADER_LIBRARIES += media_plugin_headers
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_PROPRIETARY_MODULE := true
